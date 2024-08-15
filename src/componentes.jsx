f474bea7f084247059a20385d4061d5d47804511
jorge <jvalbue5@gmail.com>
2024-08-15 08:34:28 -0500

    lo que se escribe en el input sale en la consola

--- src/componentes.jsx
+++ src/componentes.jsx
@@ -5,6 +5,7 @@ import { CiEdit } from "react-icons/ci";
 import { MdDeleteForever } from "react-icons/md";
 
 import './index.css'
+import { useState } from "react";
 
 
 
@@ -13,9 +14,8 @@ import './index.css'
 export const Header = ()=>{
     return(
     <header className="header">
-        <h1 className="header__titulo">LISTA DE DATOS</h1>
+        <h1 className="header__titulo">LISTA DE TAREAS</h1>
         <button className="header__boton"> Mostrar tareas realizadas <FaEye className="header__icono-boton"/></button>
-
     </header>
 
     )
@@ -30,14 +30,44 @@ export const Header = ()=>{
 
 
 export const Formulario = ()=>{
+
+    const [datoCapturado, setDatoCapturado] = useState('')
+
+
+
+
+    const capturaDatos = (e)=>{
+        setDatoCapturado(e.target.value)
+        console.log(datoCapturado)
+
+    }
+
+    const enviaDatoAbajo = (e)=>{
+        e.preventDefault()
+
+
+
+
+    }
+
+
+
+
     return(
 
 
-        <form className="formulario-tareas">
-            <input type="text" className="formulario-tareas__input" />
-            <button 
-                className="formulario-tareas__btn, formulario-tareas__input:focus">
-            <FaPlusSquare className="formulario-tareas__icono-btn" /></button>
+        <form className="formulario-tareas" onSubmit={enviaDatoAbajo}>
+            <input 
+                type="text"
+                value={datoCapturado} 
+                className="formulario-tareas__input" 
+                onChange={capturaDatos} 
+            />
+            
+            
+            <button className="formulario-tareas__btn">
+                <FaPlusSquare className="formulario-tareas__icono-btn" />
+            </button>
             
 
 
